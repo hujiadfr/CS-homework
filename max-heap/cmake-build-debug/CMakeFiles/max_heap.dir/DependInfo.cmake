0511085781b8b4ee6bd386c5bc07546858393034
hujiadfr <jiarun126@hotmail.com>
2021-02-21 18:14:33 +0800

    add for my compile
    add for my compile

---
+++ max-heap/cmake-build-debug/CMakeFiles/max_heap.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jiaru/Documents/GitHub/CS-homework/max-heap/heap.cpp" "/Users/jiaru/Documents/GitHub/CS-homework/max-heap/cmake-build-debug/CMakeFiles/max_heap.dir/heap.cpp.o"
+  "/Users/jiaru/Documents/GitHub/CS-homework/max-heap/main.cpp" "/Users/jiaru/Documents/GitHub/CS-homework/max-heap/cmake-build-debug/CMakeFiles/max_heap.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ max-heap/cmake-build-debug/CMakeFiles/max_heap.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/max_heap.dir/heap.cpp.o"
+  "CMakeFiles/max_heap.dir/main.cpp.o"
+  "max_heap"
+  "max_heap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/max_heap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
